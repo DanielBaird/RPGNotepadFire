33f2957fc38a2ab26b08f88b727b88adc50460b7
Daniel Baird <daniel@danielbaird.com>
2014-02-28 13:08:52 +1000

    added firebaseAngularSeed to project; set appName to 'rpgNotepad'

---
+++ app/js/services.js
@@ -0,0 +1,12 @@
+(function() {
+   'use strict';
+
+   /* Services */
+
+   angular.module('rpgNotepad.services', ['rpgNotepad.service.login', 'rpgNotepad.service.firebase'])
+
+      // put your services here!
+      // .service('serviceName', ['dependency', function(dependency) {}]);
+
+})();
+
